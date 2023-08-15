f5b4616bf61c1dcf07a0da64828cbf3fc5f34bb2
Bareet Singh <singhbareet@gmail.com>
2023-08-15 21:35:17 +0530

    sound added

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function showClock() {
     h.style.transform = `rotate(${30 * hours + minutes / 2}deg)`;
     m.style.transform = `rotate(${6 * minutes}deg)`;
     s.style.transform = `rotate(${6 * seconds}deg)`;
+
+    let sound = new Audio('sound.mp3');
+    sound.play();
 }
 
 setInterval(showClock, 1000);
